db3fa7d37a27fb8d5bc178874b426aa647836925
Isaac Lloyd <57055268+theisaaclloyd@users.noreply.github.com>
2024-10-23 13:20:20 -0400

    add server test page

--- server/index.js
+++ server/index.js
@@ -7,10 +7,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const client = mqtt.connect(process.env.MQTT_URL || 'mqtt://localhost:1883');
+const client = mqtt.connect(process.env.MQTT_URL);
 
 client.on('connect', () => {
   console.log('Connected to MQTT broker');
+});
+
+app.get('/test', (req, res) => {
+  res.end('Server is working!');
 });
 
 app.post('/update', (req, res) => {
